65f1d9c24d5bf547641797ec6f5fc3be63de66dd
pjenney58 <pete.jenney@gmail.com>
2016-02-23 16:21:53 -0500

    First local sync

--- ClassLibrary1/HttpWatcher.cs
+++ ClassLibrary1/HttpWatcher.cs
@@ -2,10 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Newtonsoft.Json.Serialization;
+
 
 namespace motInboundLib
 {
     public class HttpWatcher : inputMethod
     {
+        public void watchSite(string url)
+        {
+            //
+            // Do a timed GET against stuff, with "stuff" TBD
+            //
+        }
+
+        public HttpWatcher()
+        {
+            watchSite("http://fred.medicineontime.com/test");
+        }
+
+        public HttpWatcher(string url)
+        {
+            watchSite(url);
+        }
     }
 }
