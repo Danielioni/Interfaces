02f63bb2141e83eda122cae567793fb4fcecd5b8
pjenney58 <pete.jenney@gmail.com>
2016-08-14 01:06:23 -0400

    Phase 1 eSmartPass Integration

--- eSmartPassIntegration/ESPMainWindow.xaml.cs
+++ eSmartPassIntegration/ESPMainWindow.xaml.cs
@@ -12,6 +12,11 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using motCommonLib;
+using motOutboundLib;
+
+using System.Net;
+using System.Net.Sockets;
 
 namespace eSmartPassIntegration
 {
@@ -20,9 +25,85 @@ namespace eSmartPassIntegration
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static string __dsn;
+        private motDatabase __mainData;
+        private string __database_ip, __database_port;
+        private string __target_ip, __target_port, __target_root;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            try
+            {
+                //
+                // Get the MOT Database, it can be anywhere
+                //
+                IPAddress[] __ip_list = Dns.GetHostAddresses(Properties.Settings.Default.DB_Address);
+
+                foreach (IPAddress __ip in __ip_list)
+                {
+                    if (__ip.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        __database_ip = __ip.ToString();
+                        __database_port = Properties.Settings.Default.DB_Port;
+                    }
+                }
+
+                __dsn = string.Format("server={0};port={1};userid={2};password={3};database={4}",
+                                      __database_ip,__database_port, Properties.Settings.Default.DB_UserName, Properties.Settings.Default.DB_Password, Properties.Settings.Default.DB_DatabaseName);
+
+                __mainData = new motDatabase(__dsn, dbType.NPGServer);
+
+                //
+                // Get the eSmartPass IP
+                //
+                __ip_list = Dns.GetHostAddresses(Properties.Settings.Default.TargetURL);
+                foreach (IPAddress __ip in __ip_list)
+                {
+                    if (__ip.AddressFamily == AddressFamily.InterNetwork)
+                    {
+                        __target_ip = __ip.ToString();
+                        __target_port = Properties.Settings.Default.TargetPort;
+                        __target_root = Properties.Settings.Default.TargetRoot;
+                    }
+                }
+
+                //
+                //  All set, go play ...
+                //
+            }
+            catch (Exception e)
+            {
+                lstMain.Items.Add("Startup Error: " + e.Message);
+            }
+        }
+
+        private void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            lstMain.Items.Add("Welcome to CamptonLand!");
+            lstMain.Items.Add("Welcome to HollisLand!");
+            lstMain.Items.Add("Welcome to DennisLand!");
+        }
+
+        private void btnStop_Click(object sender, RoutedEventArgs e)
+        {
+            lstMain.Items.Clear();
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void wndMain_Activated(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstMain_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
     }
 }
--- eSmartPassIntegration/Properties/Settings.Designer.cs
+++ eSmartPassIntegration/Properties/Settings.Designer.cs
@@ -8,22 +8,138 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace eSmartPassIntegration.Properties
-{
-
-
+namespace eSmartPassIntegration.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("esmartpass.com")]
+        public string TargetURL {
+            get {
+                return ((string)(this["TargetURL"]));
+            }
+            set {
+                this["TargetURL"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("motapi")]
+        public string TargetRoot {
+            get {
+                return ((string)(this["TargetRoot"]));
+            }
+            set {
+                this["TargetRoot"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool Debug {
+            get {
+                return ((bool)(this["Debug"]));
+            }
+            set {
+                this["Debug"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool Logging {
+            get {
+                return ((bool)(this["Logging"]));
+            }
+            set {
+                this["Logging"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("192.168.1.9")]
+        public string DB_Address {
+            get {
+                return ((string)(this["DB_Address"]));
+            }
+            set {
+                this["DB_Address"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("5432")]
+        public string DB_Port {
+            get {
+                return ((string)(this["DB_Port"]));
+            }
+            set {
+                this["DB_Port"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("mot")]
+        public string DB_UserName {
+            get {
+                return ((string)(this["DB_UserName"]));
+            }
+            set {
+                this["DB_UserName"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("mot!cool")]
+        public string DB_Password {
+            get {
+                return ((string)(this["DB_Password"]));
+            }
+            set {
+                this["DB_Password"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Mot")]
+        public string DB_DatabaseName {
+            get {
+                return ((string)(this["DB_DatabaseName"]));
+            }
+            set {
+                this["DB_DatabaseName"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("80")]
+        public string TargetPort {
+            get {
+                return ((string)(this["TargetPort"]));
+            }
+            set {
+                this["TargetPort"] = value;
             }
         }
     }
