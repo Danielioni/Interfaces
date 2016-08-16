be59e2de1e09a2f07ffed9813e3a4cdc52aeba1f
lbialic <leahbialic@gmail.com>
2016-08-16 17:32:47 -0400

    initial commit of motHttpListener.cs in motClassLibrary.  please not that commemented out portion regarding the various input records will be uncommented when I start validated the input.
    Added new project motHttpInterface. This is a WPF app that starts and stops the listener and shows the data recieved.
    
    Added a unit test project motHttpListenerTests that at this point sends a simple get and post to the listener.
    
    Lots still to be done

---
+++ motHttpInterface/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using motInboundLib;
+
+namespace motHttpInterface
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        motHttpListener _listener;
+        volatile string _targetUri = string.Empty;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            tbxURI.Text = Properties.Settings.Default.TargetHttp;
+            _listener = new motHttpListener();
+            //the event handler uses the following format to allow the updates to UI from threads other than the calling thread
+            _listener.OutputTextChanged += (s, e) => Dispatcher.Invoke(() => HandlerHttpOutputChange(s, e));
+            btnStart.IsEnabled = true;
+            btnStop.IsEnabled = false;
+        }
+
+        private void ListenerStart_Click(object sender, RoutedEventArgs e)
+        {
+            
+
+            try
+            {
+                _targetUri = tbxURI.Text;
+                _listener.StartListener(_targetUri);
+                btnStart.IsEnabled = false;
+                btnStop.IsEnabled = true;
+
+
+            }
+            catch (Exception ex)
+            {
+                string _err = string.Format("An error occurred while attempting to start the Http listener: {0}\nExiting ...", ex.Message);
+               
+               
+            }
+
+        }
+
+        private void ListenerStop_Click(object sender, RoutedEventArgs e)
+        {
+            _listener.StopListener();
+            btnStart.IsEnabled = true;
+            btnStop.IsEnabled = false;
+
+        }
+
+       
+
+        private void HandlerHttpOutputChange(object sender, motHttpEventArgs e)
+        {
+            tBoxOutput.AppendText(string.Format("{0} : {1} \r\n",  e._timeStamp, e._outputMessage));
+            tBoxOutput.UpdateLayout();
+        }
+    }
+}
