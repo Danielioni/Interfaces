e4f9232f56e3d0f984e79d49d887b723cf1bf378
pjenney58 <pete.jenney@gmail.com>
2017-03-01 14:17:50 -0500

    motLegacy Database for SynMed work.

--- motGatewayTester/frmSynMed.Designer.cs
+++ motGatewayTester/frmSynMed.Designer.cs
@@ -31,24 +31,30 @@
             this.components = new System.ComponentModel.Container();
             this.btnGo = new System.Windows.Forms.Button();
             this.txtPatientLastName = new System.Windows.Forms.TextBox();
-            this.txtPatientDOB = new System.Windows.Forms.TextBox();
             this.txtCycleStartDate = new System.Windows.Forms.TextBox();
             this.txtCycleLength = new System.Windows.Forms.TextBox();
             this.txtOutFile = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.txtPatientFirstName = new System.Windows.Forms.TextBox();
             this.txtPatientMI = new System.Windows.Forms.TextBox();
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbUseNext = new System.Windows.Forms.RadioButton();
+            this.rbUseLegacy = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // btnGo
             // 
             this.btnGo.Location = new System.Drawing.Point(689, 359);
-            this.btnGo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnGo.Margin = new System.Windows.Forms.Padding(4);
             this.btnGo.Name = "btnGo";
             this.btnGo.Size = new System.Drawing.Size(249, 110);
             this.btnGo.TabIndex = 0;
@@ -58,24 +64,16 @@
             // 
             // txtPatientLastName
             // 
-            this.txtPatientLastName.Location = new System.Drawing.Point(180, 94);
-            this.txtPatientLastName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPatientLastName.Location = new System.Drawing.Point(180, 128);
+            this.txtPatientLastName.Margin = new System.Windows.Forms.Padding(4);
             this.txtPatientLastName.Name = "txtPatientLastName";
             this.txtPatientLastName.Size = new System.Drawing.Size(99, 22);
             this.txtPatientLastName.TabIndex = 1;
             // 
-            // txtPatientDOB
-            // 
-            this.txtPatientDOB.Location = new System.Drawing.Point(180, 126);
-            this.txtPatientDOB.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.txtPatientDOB.Name = "txtPatientDOB";
-            this.txtPatientDOB.Size = new System.Drawing.Size(244, 22);
-            this.txtPatientDOB.TabIndex = 2;
-            // 
             // txtCycleStartDate
             // 
             this.txtCycleStartDate.Location = new System.Drawing.Point(180, 158);
-            this.txtCycleStartDate.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtCycleStartDate.Margin = new System.Windows.Forms.Padding(4);
             this.txtCycleStartDate.Name = "txtCycleStartDate";
             this.txtCycleStartDate.Size = new System.Drawing.Size(244, 22);
             this.txtCycleStartDate.TabIndex = 3;
@@ -83,7 +81,7 @@
             // txtCycleLength
             // 
             this.txtCycleLength.Location = new System.Drawing.Point(180, 190);
-            this.txtCycleLength.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtCycleLength.Margin = new System.Windows.Forms.Padding(4);
             this.txtCycleLength.Name = "txtCycleLength";
             this.txtCycleLength.Size = new System.Drawing.Size(244, 22);
             this.txtCycleLength.TabIndex = 4;
@@ -91,7 +89,7 @@
             // txtOutFile
             // 
             this.txtOutFile.Location = new System.Drawing.Point(180, 222);
-            this.txtOutFile.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtOutFile.Margin = new System.Windows.Forms.Padding(4);
             this.txtOutFile.Name = "txtOutFile";
             this.txtOutFile.Size = new System.Drawing.Size(244, 22);
             this.txtOutFile.TabIndex = 5;
@@ -99,22 +97,12 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(79, 97);
+            this.label1.Location = new System.Drawing.Point(79, 131);
             this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(93, 17);
             this.label1.TabIndex = 6;
             this.label1.Text = "Patient Name";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(85, 129);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(86, 17);
-            this.label2.TabIndex = 7;
-            this.label2.Text = "Patient DOB";
             // 
             // label3
             // 
@@ -148,7 +136,7 @@
             // 
             // txtPatientFirstName
             // 
-            this.txtPatientFirstName.Location = new System.Drawing.Point(287, 94);
+            this.txtPatientFirstName.Location = new System.Drawing.Point(287, 128);
             this.txtPatientFirstName.Margin = new System.Windows.Forms.Padding(4);
             this.txtPatientFirstName.Name = "txtPatientFirstName";
             this.txtPatientFirstName.Size = new System.Drawing.Size(99, 22);
@@ -156,7 +144,7 @@
             // 
             // txtPatientMI
             // 
-            this.txtPatientMI.Location = new System.Drawing.Point(394, 94);
+            this.txtPatientMI.Location = new System.Drawing.Point(394, 128);
             this.txtPatientMI.Margin = new System.Windows.Forms.Padding(4);
             this.txtPatientMI.Name = "txtPatientMI";
             this.txtPatientMI.Size = new System.Drawing.Size(26, 22);
@@ -167,27 +155,102 @@
             this.notifyIcon1.Text = "notifyIcon1";
             this.notifyIcon1.Visible = true;
             // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(637, 125);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(270, 22);
+            this.txtUserName.TabIndex = 13;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(637, 153);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtPassword.Size = new System.Drawing.Size(270, 22);
+            this.txtPassword.TabIndex = 14;
+            this.txtPassword.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(537, 125);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 17);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "User Name";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(537, 153);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 17);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Password";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbUseLegacy);
+            this.groupBox1.Controls.Add(this.rbUseNext);
+            this.groupBox1.Location = new System.Drawing.Point(82, 328);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 100);
+            this.groupBox1.TabIndex = 17;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "[ Database ]";
+            // 
+            // rbUseNext
+            // 
+            this.rbUseNext.AutoSize = true;
+            this.rbUseNext.Location = new System.Drawing.Point(25, 31);
+            this.rbUseNext.Name = "rbUseNext";
+            this.rbUseNext.Size = new System.Drawing.Size(109, 21);
+            this.rbUseNext.TabIndex = 0;
+            this.rbUseNext.Text = "Use motNext";
+            this.rbUseNext.UseVisualStyleBackColor = true;
+            // 
+            // rbUseLegacy
+            // 
+            this.rbUseLegacy.AutoSize = true;
+            this.rbUseLegacy.Checked = true;
+            this.rbUseLegacy.Location = new System.Drawing.Point(25, 59);
+            this.rbUseLegacy.Name = "rbUseLegacy";
+            this.rbUseLegacy.Size = new System.Drawing.Size(127, 21);
+            this.rbUseLegacy.TabIndex = 1;
+            this.rbUseLegacy.TabStop = true;
+            this.rbUseLegacy.Text = "Use motLegacy";
+            this.rbUseLegacy.UseVisualStyleBackColor = true;
+            // 
             // frmSynMed
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(955, 510);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUserName);
             this.Controls.Add(this.txtPatientMI);
             this.Controls.Add(this.txtPatientFirstName);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtOutFile);
             this.Controls.Add(this.txtCycleLength);
             this.Controls.Add(this.txtCycleStartDate);
-            this.Controls.Add(this.txtPatientDOB);
             this.Controls.Add(this.txtPatientLastName);
             this.Controls.Add(this.btnGo);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "frmSynMed";
             this.Text = "frmSynMed";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -197,17 +260,22 @@
 
         private System.Windows.Forms.Button btnGo;
         private System.Windows.Forms.TextBox txtPatientLastName;
-        private System.Windows.Forms.TextBox txtPatientDOB;
         private System.Windows.Forms.TextBox txtCycleStartDate;
         private System.Windows.Forms.TextBox txtCycleLength;
         private System.Windows.Forms.TextBox txtOutFile;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.TextBox txtPatientFirstName;
         private System.Windows.Forms.TextBox txtPatientMI;
         private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbUseLegacy;
+        private System.Windows.Forms.RadioButton rbUseNext;
     }
 }
--- motGatewayTester/frmSynMed.cs
+++ motGatewayTester/frmSynMed.cs
@@ -8,32 +8,78 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using motOutboundLib;
+using motMatchineInterface;
 
 namespace motGatewayTester
 {
     public partial class frmSynMed : Form
     {
+        public static string __start_date = string.Empty;
+        public static string __patient_last_name = string.Empty;
+        public static string __patient_first_name = string.Empty;
+        public static string __patient_middle_initial = string.Empty;
+        public static string __cycle_start;
+        public static string __facility_name = string.Empty;
+
+        public static string __username = string.Empty;
+        public static string __password = string.Empty;
+
+        public static bool useLegacy = true;
+
         public frmSynMed()
         {
-            InitializeComponent();           
+            InitializeComponent();
         }
 
         private void btnGo_Click(object sender, EventArgs e)
         {
-            __get_started();   
+            __start_date = txtCycleStartDate.Text;
+            __patient_last_name = txtPatientLastName.Text;
+            __patient_first_name = txtPatientFirstName.Text;
+            __patient_middle_initial = txtPatientMI.Text;
+
+            __username = txtUserName.Text;
+            __password = txtPassword.Text;
+
+            useLegacy = rbUseLegacy.Checked;
+
+            __get_started();
         }
 
         private static async void __get_started()
         {
             try
             {
-                motNextSynMed __synmed = new motNextSynMed(@"\motNext\SynmedFiles");
-                DateTime __due = DateTime.Parse("2/10/2017");
+                DateTime __due;
 
-                await __synmed.Login("mot", "mot");
-                await __synmed.WriteCycle(__due);
+               
+                if (!useLegacy)
+                {
+                    motNextSynMed __synmed = new motNextSynMed(@"\motNext\SynmedFiles");
 
-                //await __synmed.WriteCycle(__due);
+                    await __synmed.Login(__username, __password);
+
+                    await __synmed.BuildPatientList(2000);
+
+                    if (!string.IsNullOrEmpty(__start_date) && !string.IsNullOrEmpty(__patient_last_name))
+                    {
+                        await __synmed.WritePatient(__patient_last_name, __patient_first_name, __patient_middle_initial, DateTime.Parse(__start_date), 30);
+                    }
+
+                    else if (!string.IsNullOrEmpty(__patient_last_name))
+                    {
+                        await __synmed.WriteCycle(DateTime.Parse(__start_date));
+                    }
+                }
+                else
+                {
+                    motLegacySynMed __old_synmed = new motLegacySynMed(@"\motNext\SynmedFiles");
+
+                    await __old_synmed.Login(__username, __password);
+                    await __old_synmed.WriteCycle(DateTime.Parse(__start_date));
+                }
+
+
 
                 /*
                 await __synmed.Write(@"Jenney",
@@ -43,20 +89,30 @@ namespace motGatewayTester
                                     __due,
                                      30);
                 
-  */
+
                 await __synmed.WritePatient(@"ALLEN",
                                      @"PRISCILLA",
                                      @"",
-                                    DateTime.Parse("12/1/1960"),
+                                    DateTime.Parse("12/31/1960"),
                                     __due,
                                     30);
-             
+               */
 
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
             }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
         }
     }
 }
